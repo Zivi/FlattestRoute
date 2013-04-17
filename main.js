1f4cfb3bd84f4de245cec9713c88f371d54e133f
Zivi Weinstock <zivi.weinstock@gmail.com>
2013-04-16 17:10:00 -0700

    seventh commit, with elevation code but not working

--- main.js
+++ main.js
@@ -14,11 +14,19 @@ $(function() {
 //API key: AIzaSyAQu2QTu2fe1zuir1GUEW8pai7sTnxmbsg
 
 //at initialization
-
-
 var directionsDisplay = null;
 var DirectionsService = new google.maps.DirectionsService();
 var map = null;
+
+//set variables for elevation
+var elevator = null;
+var chart = null;
+var infowindow = new google.maps.InfoWindow();
+var polyline
+
+//load the visualization API with the columnchart package
+google.load("visualization", "1", {packages: ["columnchart"]});
+
 
 
 function initialize_maps() {
@@ -39,8 +47,71 @@ function initialize_maps() {
 	map = new google.maps.Map(mapCanvas, mapOptions);
 	directionsDisplay.setMap(map);
 	//populate panel with written directions
-	directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+	directionsDisplay.setPanel($("directionsPanel"));
+
+	//add elevation service
+	elevator = new google.maps.ElevationService();
+
+	//draw the path with the visualization api and elevation service
+	drawPath();
 }
+
+function drawPath() {
+	//create a new chart in the elevation chart div
+	chart = new google.visualization.ColumnChart($("#elevation_chart"));
+	var path = [start, end]; //not sure what to put here
+
+	//create a path elevation request object with path
+	//need to figure out how to get the path length and divide the samples
+	//up by unit such as 100m
+	var pathRequest = {
+		'path': path,
+		'samples': 256
+	}
+	//initiate the path request
+	elevator.getElevationAlongPath(pathRequest, plotElevation);
+}
+
+//take an array of elevation result objects, draws a path on the map
+//and plots the elevation profile on the chart
+function plotElevation(results, status) {
+	if (status == google.maps.ElevationStatus.OK) {
+		elevations = results;
+	}
+	//extract elevation samples from returned results
+	var elevationPath = [];
+	for (var i = 0; i < results.length; i++) {
+		elevationPath.push(elevations[i].location);
+	}
+
+	//display the line of the elevation path
+	var pathOptions = {
+		path: elevationPath,
+		strokeColor: '#0000CC',
+		opacity: 0.4,
+		map: map
+	}
+	polyline = new google.maps.Polyline(pathOptions);
+
+	//extract the data to populate the chart
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Sample');
+	data.addColumn('number', 'Elevation');
+	for (var i = 0; i < results.length; i++) {
+		data.addRow(['', elevations[i].elevation]);
+	}
+
+	//draw teh chart using the data within its div
+	$("#elevation_chart").style.display = 'block';
+	chart.draw(data, {
+		width: 640,
+		height: 200,
+		legend: 'none',
+		titleY: 'Elevation (m)'
+	});
+}
+
+
 
 function calcRoute() {
 	//create object directions service
